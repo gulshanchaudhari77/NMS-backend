82d2dd84b7d05633b9caeae2f5a53527c290bf97
Gulshan.DEV <132228055+gulshanchaudhari77@users.noreply.github.com>
2025-05-05 20:12:19 +0530

    editing some bcrytp

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,7 @@
 const User = require("../model/User");
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 router.post("/", async (req, res) => {
--- routes/signup.js
+++ routes/signup.js
@@ -1,6 +1,6 @@
 const User = require("../model/User");
 const express = require("express");
- const bcrypt = require("bcrypt"); // Import bcrypt
+ const bcrypt = require("bcryptjs"); // Import bcrypt
 
 const router = express.Router();
 
